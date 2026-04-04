ca20f381fbb91dc7d4ee0952797ee772805544b4
jayjanii <jay_jani@icloud.com>
2026-04-03 22:20:44 -0700

    initial commit

---
+++ notebook.py
@@ -0,0 +1,23 @@
+import marimo
+
+__generated_with = "0.22.4"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import torch
+    from transformers import ViTForImageClassification, AutoImageProcessor
+    from PIL import Image
+    import requests
+
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
